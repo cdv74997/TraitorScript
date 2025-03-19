e24ca10d85a71757f8e7d0ac1511d25376e612ce
Elena-Ramos <elena.ramos.717@my.csun.edu>
2025-03-19 15:40:01 -0700

    tryReadSymbol method written, not yet tested

--- src/main/java/tokenizer/Tokenizer.java
+++ src/main/java/tokenizer/Tokenizer.java
@@ -1,5 +1,7 @@
 package tokenizer;
 
+import java.util.Optional;
+
 public class Tokenizer{
     public final String input;
     private int position;
@@ -18,7 +20,63 @@ public class Tokenizer{
                Character.isWhitespace(input.charAt(position))) {
             position++;
         }
-    }
+    } // skipWhitespace
 
+    public Optional<Token> tryReadSymbol(){
+        if(input.startsWith(",", position)){
+            position++;
+            return Optional.of(new CommaToken());
+        } else if(input.startsWith("(", position)){
+            position++;
+            return Optional.of(new LParenToken());
+        } else if(input.startsWith(")", position)){
+            position++;
+            return Optional.of(new RParenToken());
+        } else if(input.startsWith(":", position)){
+            position++;
+            return Optional.of(new CommaToken());
+        } else if(input.startsWith("{", position)){
+            position++;
+            return Optional.of(new LCurlyToken());
+        } else if(input.startsWith("}", position)){
+            position++;
+            return Optional.of(new RCurlyToken());
+        } else if(input.startsWith(";", position)){
+            position++;
+            return Optional.of(new SemicolonToken());
+        } else if(input.startsWith(".", position)){
+            position++;
+            return Optional.of(new DotToken());
+        } else if(input.startsWith("*", position)){
+            position++;
+            return Optional.of(new StarToken());
+        } else if(input.startsWith("/", position)){
+            position++;
+            return Optional.of(new SlashToken());
+        } else if(input.startsWith("+", position)){
+            position++;
+            return Optional.of(new PlusToken());
+        } else if(input.startsWith("-", position)){
+            position++;
+            return Optional.of(new MinusToken());
+        } else if(input.startsWith("<", position)){
+            position++;
+            return Optional.of(new LessThanToken());
+        } else if(input.startsWith("=>", position)){
+            position++;
+            return Optional.of(new RArrowToken());
+        } else if(input.startsWith("==", position)){
+            position++;
+            return Optional.of(new DEqualsToken());
+        } else if(input.startsWith("!=", position)){
+            position++;
+            return Optional.of(new NEqualsToken());
+        } else if(input.startsWith("=", position)){
+            position++;
+            return Optional.of(new EqualsToken());
+        } else{
+            return Optional.empty();
+        }
+    } // tryReadSymbol
     
 }

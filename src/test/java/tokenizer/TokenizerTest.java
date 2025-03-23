6d02427e9e5a815ffa579c90d48e1028bfad71bd
Elena-Ramos <elena.ramos.717@my.csun.edu>
2025-03-22 23:18:46 -0700

    tests for symbol tokens

--- src/test/java/tokenizer/TokenizerTest.java
+++ src/test/java/tokenizer/TokenizerTest.java
@@ -28,4 +28,106 @@ public class TokenizerTest{
         tokenizer.skipWhitespace();
         assertEquals(1, tokenizer.getPosition());
     }
+
+    @Test
+    public void testReadCommaToken(){
+        final Tokenizer tokenizer = new Tokenizer(",");
+        assertEquals(Optional.of(new CommaToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadLParenToken(){
+        final Tokenizer tokenizer = new Tokenizer("(");
+        assertEquals(Optional.of(new LParenToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadRParenToken(){
+        final Tokenizer tokenizer = new Tokenizer(")");
+        assertEquals(Optional.of(new RParenToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadColonToken(){
+        final Tokenizer tokenizer = new Tokenizer(":");
+        assertEquals(Optional.of(new ColonToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadLCurlyToken(){
+        final Tokenizer tokenizer = new Tokenizer("{");
+        assertEquals(Optional.of(new LCurlyToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadRCurlyToken(){
+        final Tokenizer tokenizer = new Tokenizer("}");
+        assertEquals(Optional.of(new RCurlyToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadSemicolonToken(){
+        final Tokenizer tokenizer = new Tokenizer(";");
+        assertEquals(Optional.of(new SemicolonToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadDotToken(){
+        final Tokenizer tokenizer = new Tokenizer(".");
+        assertEquals(Optional.of(new DotToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadStarToken(){
+        final Tokenizer tokenizer = new Tokenizer("*");
+        assertEquals(Optional.of(new StarToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadSlashToken(){
+        final Tokenizer tokenizer = new Tokenizer("/");
+        assertEquals(Optional.of(new SlashToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadPlusToken(){
+        final Tokenizer tokenizer = new Tokenizer("+");
+        assertEquals(Optional.of(new PlusToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadMinusToken(){
+        final Tokenizer tokenizer = new Tokenizer("-");
+        assertEquals(Optional.of(new MinusToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadLessThanToken(){
+        final Tokenizer tokenizer = new Tokenizer("<");
+        assertEquals(Optional.of(new LessThanToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadRArrowToken(){
+        final Tokenizer tokenizer = new Tokenizer("=>");
+        assertEquals(Optional.of(new RArrowToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadDEqualsToken(){
+        final Tokenizer tokenizer = new Tokenizer("==");
+        assertEquals(Optional.of(new DEqualsToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadNEqualsToken(){
+        final Tokenizer tokenizer = new Tokenizer("!=");
+        assertEquals(Optional.of(new NEqualsToken()), tokenizer.tryReadSymbol());
+    }
+
+    @Test
+    public void testReadEqualsToken(){
+        final Tokenizer tokenizer = new Tokenizer("=");
+        assertEquals(Optional.of(new EqualsToken()), tokenizer.tryReadSymbol());
+    }
 }

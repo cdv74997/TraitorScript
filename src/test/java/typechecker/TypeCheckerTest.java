a13603325f273739d81212bdea331278556443d0
cdv74997 <christian.vega.720@my.csun.edu>
2025-04-14 13:37:27 -0700

    TypeCheckerest Outline added

--- src/test/java/typechecker/TypeCheckerTest.java
+++ src/test/java/typechecker/TypeCheckerTest.java
@@ -6,3 +6,66 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.Test; 
+
+@Test 
+public void testIntegerLiteralType(){ }
+
+@Test 
+public void testBooleanLiteralType(){ }
+
+@Test 
+public void testSelfReferenceInTrait(){ }
+
+@Test 
+public void testStructInstantiationWithCorrectFields(){ }
+
+@Test 
+public void testStructMissingFieldFails(){ }
+
+@Test 
+public void testStructFieldAccessTypeCorrectness(){ }
+
+@Test 
+public void testTraitImplementationForStruct(){}
+
+@Test 
+public void testTraitMethodTypeResolution(){}
+
+@Test 
+public void testTraitMethodCallOnStructInstance(){ }
+
+@Test 
+public void testFunctionOverloadResolvesCorrectly() { }
+
+@Test 
+public void testAmbiguousOverloadCausesError() { }
+
+@Test 
+public void testHigherOrderFunctionParameterType() { }
+
+@Test 
+public void testFunctionReturningFunctionType() { }
+
+@Test 
+public void testArithmeticExpressionType() {  }
+
+@Test 
+public void testComparisonExpressionReturnsBoolean() {  }
+
+@Test 
+public void testMethodCallExpressionTypeCorrectness() { }
+
+@Test 
+public void testIfStatementTypeCheck() { }
+
+@Test 
+public void testReturnTypeMatchesFunction() { }
+
+@Test 
+public void testVoidFunctionDoesNotReturnValue() {  }
+
+@Test 
+public void testFullProgramTypeCheckPasses() { }
+
+@Test 
+public void testProgramWithTypeErrorFails() { }
